3f2c8e670e3c5bef88fbeefa326ad4d3c4e72d6e
MH Shakil <mhshakil06@gmail.com>
2024-06-19 13:29:49 +0600

    login function done

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,11 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\AuthRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-    public function login(AuthRequest $request){
-        return $request;
+    final public function login(AuthRequest $request){
+        $user = (new User())->getUserByEmailOrPhone($request->all());
+
+
+        if($user && Hash::check($request->input('password'), $user->password)){
+            $user_data['token'] = $user->createToken($user->email)->plainTextToken;
+            $user_data['name'] = $user->name;
+            $user_data['phone'] = $user->phone;
+            $user_data['email'] = $user->email;
+            $user_data['photo'] = $user->photo;
+            return response()->json($user_data);
+        }
+        throw ValidationException::withMessages([
+            'email' => ['The Provided credentials are incorrect']
+        ]);
     }
 }
